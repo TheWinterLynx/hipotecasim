bcb1dc0613b32abfbac56189dd7113f8970e9ce7
Javier Cuadrado <javier.cuadrado@outlook.com>
2026-04-18 12:23:59 +0200

    Remove some comment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/hipotecasim/', // cambia esto por el nombre real del repo
+    base: '/hipotecasim/', 
 })
